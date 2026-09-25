38927dd7bf1e1ba6edee62711d4305f934c9c809
Deepanshu Verma <deepanshusv3@gmail.com>
2026-09-26 04:12:49 +0530

    feat: configure API base URL using environment variable and ignore .env file

--- src/api/api-config.jsx
+++ src/api/api-config.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://api.cabnex.in",
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
